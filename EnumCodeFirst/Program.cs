20bb03fb47e6f8a866d9f3dac91653facf8ff2b0
Andrzej Ołdak <Andrzej.Oldak@luxmed.pl>
2016-09-23 16:48:40 +0200

    Enum Code First

---
+++ EnumCodeFirst/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Data.Entity;
+using System.Linq;
+
+namespace EnumCodeFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new EnumTestContext())
+            {
+                context.Departments.Add(new Department { Name = DepartmentNames.English });
+
+                context.SaveChanges();
+
+                var department = (from d in context.Departments
+                                  where d.Name == DepartmentNames.English
+                                  select d).FirstOrDefault();
+
+                Console.WriteLine(
+                    "DepartmentID: {0} Name: {1}",
+                    department.DepartmentID,
+                    department.Name);
+            }
+        }
+    }
+
+    public enum DepartmentNames
+    {
+        English,
+        Math,
+        Economics
+    }
+
+    public partial class Department
+    {
+        public int DepartmentID { get; set; }
+        public DepartmentNames Name { get; set; }
+        public decimal Budget { get; set; }
+    }
+
+    public partial class EnumTestContext : DbContext
+    {
+        public DbSet<Department> Departments { get; set; }
+    }
+}
